bebed31fadbbf335dc38dbaacf59032055cd96d9
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-29 15:02:52 +0400

    Adding Person calss with it's Attributes and Constructor.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public abstract class Person
+    {
+        // Attributes:
+
+        public string Name { get;private set; }
+        public int Age { get;private set; }   
+        public string Gender { get;private set; }
+
+        //Constructor
+        public Person(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+      
+
+    }
+}
