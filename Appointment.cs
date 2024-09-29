c0e2dbe0cef4b5c4f3902c7d415a77982c10e14e
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-29 15:57:36 +0400

    Adding Appointment class with its Attributes and Constructor.

---
+++ Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Appointment
+    {
+        //Attributes
+
+        public Patient Patient;
+        public Doctor Doctor;
+        public DateTime AppointmentDate;
+
+        public Appointment(Patient patient, Doctor doctor, DateTime DT)
+        { 
+            Patient = patient;
+            Doctor = doctor;
+            AppointmentDate = DT;
+        }
+
+    }
+}
