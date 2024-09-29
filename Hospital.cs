016127a1b9f093edcbfe6f58a01482a4565e291d
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-29 16:07:45 +0400

    Adding Hospital Class with it's Attributes and Constructor.

---
+++ Hospital.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Hospital
+    {
+        //Attributes
+        public List<Doctor> DoctorsList = new List<Doctor>();
+        public List<Patient> PatientsList = new List<Patient>();
+        public List<Room> RoomsList = new List<Room>();
+
+        //Constructor
+        public Hospital(List<Doctor> doctors, List<Patient>patients,List<Room>room) 
+        { 
+            DoctorsList = doctors;
+            PatientsList = patients;
+            RoomsList = room;
+        }
+
+
+
+    }
+}
