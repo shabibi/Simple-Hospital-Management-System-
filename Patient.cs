f360650286ed119d66018f0710e9d1197959686b
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-29 15:21:35 +0400

    Changing the gender to be only to options (male, female)

---
+++ Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Patient : Person
+    {
+        public int Id { get;private set; }
+        public string Ailment;
+        public string AssignedDoctor;
+
+
+        public Patient(string name, int age, Gender gender, string doctor):base(name,age,gender)
+        { 
+            AssignedDoctor = doctor;
+        }
+
+    }
+}
