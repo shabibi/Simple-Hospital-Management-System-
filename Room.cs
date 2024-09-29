d2df44f44499583ef2fc80f37c5e3f39873e3603
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-29 15:47:03 +0400

    Adding Room Class with it's Attributes and Constructor.

---
+++ Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Room
+    {
+        public int RoomNumber;
+        public enum RoomTypes { General, ICU, OperationTheater }
+        public RoomTypes RoomType;
+        protected bool IsOccupied = false;
+
+        public Room(int RNumber , RoomTypes Rtype )
+        { 
+            RoomNumber = RNumber;
+            RoomType = Rtype;
+        }
+
+    }
+}
