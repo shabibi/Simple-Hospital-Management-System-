601f9816cff412e5e4c8a0777402ab384550ce53
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-29 15:35:35 +0400

    Adding Doctor Class with it's Attributes and Constructor.

---
+++ Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Doctor : Person
+    {
+        //Attributes
+        public int DoctorID { get;private set; }
+        public string Specialization { get;private set; }
+        public List<Patient> patients = new List<Patient>();
+
+       //Constructor
+        public Doctor(string name, int age, Gender gender, int doctorID, string specialization) : base(name, age, gender)
+        {
+            DoctorID = doctorID;
+            Specialization = specialization;
+        }
+    }
+}
