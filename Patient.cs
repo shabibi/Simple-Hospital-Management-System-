068c927b8d136983b61a7abc2935d09a05d32f2a
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-30 09:00:09 +0400

    Adding DisplayInfo method in Patient class.

--- Patient.cs
+++ Patient.cs
@@ -9,16 +9,29 @@ namespace SimpleHospitalManagementSystem
 
     public class Patient : Person
     {
+        //Attributes
         public int Id { get; private set; }
         public string Ailment;
         public Doctor AssignedDoctor; 
-        public Room room; 
+        public Room room;
 
-
-        public Patient(string name, int age, Gender gender, Doctor doctor) : base(name, age, gender)
+        //Constructor: Initialize attributes and assign a doctor.
+        public Patient(string name, int age, Gender gender,string ailment, Doctor doctor) : base(name, age, gender)
         {
+            Ailment = ailment;
             AssignedDoctor = doctor;
         }
+
+        //Methods
+
+        //Override the DisplayInfo() method to include PatientID and Ailment.
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"PatientID : {Id}, Ailment : {Ailment}, Doctor : {Ailment}");
+            
+        }
+
+
 
     }
 
