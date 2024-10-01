3836863593c837dd33584bfcc52dd257151b4cb7
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-01 10:28:32 +0400

    Adding Clinic class with it's Attributes.

---
+++ Clinic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Clinic
+    {
+        //Attributes
+        public int ClinicID { get;private set; }
+        public string ClinicName { get;private set; }
+        public enum Specializations { Cardiology, Neurology, Dermatology }
+        public Specializations Specialization;
+        public List<Room> Rooms = new List<Room>();
+        public Dictionary<Doctor,List<Appointment>> AvailableAppointments = new Dictionary<Doctor,List<Appointment>>();
+
+
+
+
+
+
+    }
+}
