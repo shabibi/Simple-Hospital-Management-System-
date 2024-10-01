e5a4509ef0c1b55ad64d9a99ec7114769c3e3e87
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-01 10:53:48 +0400

    Adding InPatient class with it's attributes and Methods , Modifing Patient class.

---
+++ InPatient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class InPatient : Patient
+    {
+        public Doctor AssignedDoctor;
+        public Room room;
+        public DateTime AdmissionDate;
+
+        public InPatient (int id, string name, int age, Gender gender, string ailment, Doctor assignedDoctor, Room room, DateTime AdmissionDate) : base(id,name,age,gender,ailment)
+        {
+            this.AssignedDoctor = assignedDoctor;
+            this.room = room;
+            this.AdmissionDate = AdmissionDate;
+        }
+
+        //Methods
+        //AssignRoom(Room room): Assigns a room to the patient.
+        public void AssignRoom(Room room)
+        {
+            if (!room.VacateRoom())
+            {
+                room.OccupyRoom();
+                this.room = room;
+
+                Console.WriteLine($"{Name}  assigned to room {room.RoomNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Room not availble.");
+            }
+        }
+
+        // Marks the patient as discharged (set the room to null).
+        public void Discharge()
+        {
+            room.VacateRoom();
+            room = null;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Doctor : {AssignedDoctor}, Room : {room.RoomNumber}, AdmissionDate: {AdmissionDate.ToString()}");
+
+        }
+    }
+}
--- Patient.cs
+++ Patient.cs
@@ -12,39 +12,13 @@ namespace SimpleHospitalManagementSystem
         //Attributes
         public int Id { get; private set; }
         public string Ailment;
-        public Doctor AssignedDoctor; 
-        public Room room;
+        
 
         //Constructor: Initialize attributes and assign a doctor.
-        public Patient(int  id ,string name, int age, Gender gender,string ailment, Doctor doctor) : base(name, age, gender)
+        public Patient(int  id ,string name, int age, Gender gender,string ailment) : base(name, age, gender)
         {
             Id = id;
             Ailment = ailment;
-            AssignedDoctor = doctor;
-        }
-
-        //Methods
-        //AssignRoom(Room room): Assigns a room to the patient.
-        public void AssignRoom(Room room)
-        {
-            if (!room.VacateRoom())
-            {
-                room.OccupyRoom();
-                this.room = room;
-
-                Console.WriteLine($"{Name}  assigned to room {room.RoomNumber}");
-            }
-            else
-            {
-                Console.WriteLine("Room not availble.");
-            }
-        }
-
-        // Marks the patient as discharged (set the room to null).
-        public void Discharge()
-        {
-            room.VacateRoom();
-            room = null;
             
         }
 
@@ -52,7 +26,7 @@ namespace SimpleHospitalManagementSystem
         public override void DisplayInfo()
         {
             base.DisplayInfo();
-            Console.WriteLine($"PatientID : {Id}, Ailment : {Ailment}, Doctor : {AssignedDoctor.Name}");
+            Console.WriteLine($"PatientID : {Id}, Ailment : {Ailment}");
             
         }
 
