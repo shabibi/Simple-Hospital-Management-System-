a6366ede236411488d0a3e0f923635bb77a67b23
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-01 11:00:08 +0400

    Adding OutPatient class.

---
+++ OutPatient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class OutPatient : Patient
+    {
+        public Clinic ClinicAssigned;
+
+        public OutPatient(int id, string name, int age, Gender gender, string ailment, Clinic ClinicAssigned) : base(id, name, age, gender, ailment)
+        {
+            this.ClinicAssigned = ClinicAssigned;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Doctor :{ClinicAssigned.ClinicName }");
+        }
+    }
+}
