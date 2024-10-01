1314fbb3acd729b5923a54da0e8c884a8b75ff0f
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-01 13:48:33 +0400

    Adding AddRoom and AddAvailableAppointment methods in Clinic class.

--- Clinic.cs
+++ Clinic.cs
@@ -16,6 +16,35 @@ namespace SimpleHospitalManagementSystem
         public List<Room> Rooms = new List<Room>();
         public Dictionary<Doctor,List<Appointment>> AvailableAppointments = new Dictionary<Doctor,List<Appointment>>();
 
+        //Methods
+        //Adds rooms to the clinic.
+        public void AddRoom (Room room)
+        {
+            if (room != null)
+                Rooms.Add(room);
+            else
+                Console.WriteLine("Invalid input..");
+        }
+
+        // Generates appointment slots for the doctor on a specified day for a given period and adds them to the clinic.
+        public void AddAvailableAppointment(Doctor doctor, DateTime appointmentDay, TimeSpan period)
+        {
+            if (!AvailableAppointments.ContainsKey(doctor))
+            {
+                AvailableAppointments[doctor] = new List<Appointment>();
+            }
+            TimeSpan startHour = new TimeSpan(9,0,0);
+            TimeSpan endHour = startHour + period;
+            for (TimeSpan i = startHour; i <= endHour; i.Add(new TimeSpan(1,0,0)))
+            {
+                Appointment appointment = new Appointment();
+                appointment.ScheduleAppointment(null, appointmentDay,i);
+                AvailableAppointments[doctor].Add(appointment);
+            }
+            
+        }
+
+        
 
 
 
