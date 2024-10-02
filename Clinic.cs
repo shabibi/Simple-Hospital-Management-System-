b659fb4d43210d7053feba5df750e4f4b5bb2405
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-02 15:43:17 +0400

    update Clinic class and bug fixing.

--- Clinic.cs
+++ Clinic.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SimpleHospitalManagementSystem
 {
+    public enum Specializations { Cardiology, Neurology, Dermatology }
     public class Clinic
     {
         //Attributes
         public int ClinicID { get;private set; }
         public string ClinicName { get;private set; }
-        public enum Specializations { Cardiology, Neurology, Dermatology }
+
         public Specializations Specialization;
         public List<Room> Rooms = new List<Room>();
         public Dictionary<Doctor,List<Appointment>> AvailableAppointments = new Dictionary<Doctor,List<Appointment>>();
@@ -29,7 +32,10 @@ namespace SimpleHospitalManagementSystem
         public void AddRoom (Room room)
         {
             if (room != null)
+            {
                 Rooms.Add(room);
+                room.OccupyRoom();
+            }
             else
                 Console.WriteLine("Invalid input..");
         }
@@ -37,36 +43,46 @@ namespace SimpleHospitalManagementSystem
         // Generates appointment slots for the doctor on a specified day for a given period and adds them to the clinic.
         public void AddAvailableAppointment(Doctor doctor, DateTime appointmentDay, TimeSpan period)
         {
+            TimeSpan start = new TimeSpan(9, 0, 0);
             if (!AvailableAppointments.ContainsKey(doctor))
             {
                 AvailableAppointments[doctor] = new List<Appointment>();
+                for (int i = 0; i < period.TotalHours; i++)
+                {
+                    Appointment appointment = new Appointment();
+                    appointment.ScheduleAppointment(null, appointmentDay, start.Add(new TimeSpan(i, 0, 0)),false);
+                    AvailableAppointments[doctor].Add(appointment);
+                    Console.WriteLine($"Appointment Scheduled for {doctor.Name} in {appointmentDay.AddHours(i).ToString("yyy-MM-dd")}  at {start.Add(new TimeSpan(i, 0, 0))}");
+                }
             }
-            TimeSpan startHour = new TimeSpan(9,0,0);
-            TimeSpan endHour = startHour + period;
-            for (TimeSpan i = startHour; i <= endHour; i.Add(new TimeSpan(1,0,0)))
+            else
             {
-                Appointment appointment = new Appointment();
-                appointment.ScheduleAppointment(null, appointmentDay,i);
-                AvailableAppointments[doctor].Add(appointment);
+                Console.WriteLine($"{doctor.Name} Not Available to  Schedule Appointment in this Clinic");
             }
-            
+           
+            Console.WriteLine("**************************************************");
+
+
+
         }
 
         public void BookAppointment (Patient patient, Doctor doctor, DateTime appointmentDay,TimeSpan appointmentTime)
         {
+            Console.WriteLine("**************Book Appointment***********************");
             bool flage = false;
             if (AvailableAppointments.ContainsKey(doctor))
             {
                 List<Appointment> appointments = AvailableAppointments[doctor];
                 for (int i = 0; i < AvailableAppointments[doctor].Count; i++)
                 {
-                    if (!appointments[i].IsBooked)
+                    if (!appointments[i].IsBooked && appointments[i].AppointmentTime == appointmentTime && appointments[i].AppointmentDate == appointmentDay)
                     {
-                        appointments[i].ScheduleAppointment(patient, appointmentDay, appointmentTime);
+                        appointments[i].ScheduleAppointment(patient, appointmentDay, appointmentTime,true);
+                        Console.WriteLine($"{patient.Name} Assigned appointment on {appointmentDay.ToString("yyy-MM-dd")} at {appointmentTime}");
                         flage = true;
                     }
                 }
-                if(flage != true)
+                if (flage != true)
                     Console.WriteLine("Selected appointment is not available.");
             }
             else
@@ -75,16 +91,47 @@ namespace SimpleHospitalManagementSystem
             }
         }
 
+        public void CancelAppointment(Patient patient,Doctor doctor, DateTime dateTime,TimeSpan time)
+        {
+            Console.WriteLine("**************Cancel Appointment***********************");
+            if (AvailableAppointments.ContainsKey(doctor))
+            {
+                bool flage = false;
+                List<Appointment> appointments = AvailableAppointments[doctor];
+                for (int i = 0; i < AvailableAppointments[doctor].Count; i++)
+                {
+                    if (appointments[i].IsBooked && appointments[i].AppointmentTime == time && appointments[i].AppointmentDate == dateTime)
+                    {
+                        
+                        appointments[i].CancelAppointment( patient, dateTime,  time);
+                        Console.WriteLine($"{patient.Name} Canced appointment on {appointments[i].AppointmentDate.ToString("yyy-MM-dd")} at {appointments[i].AppointmentTime}");
+                        flage = true;
+                    }
+                }
+                if (flage != true)
+                    Console.WriteLine("Selected appointment is not available.");
+
+            }
+            else
+            {
+                Console.WriteLine("Doctor Not Found..");
+            }
+
+        }
+
         public void DisplayAvailableAppointments()
         {
+           
+            Console.WriteLine("****************Display Available Appointments****************\n");
             foreach (var doctors in AvailableAppointments.Keys)
             {
-                Console.WriteLine(doctors.ToString());
+                Console.WriteLine(doctors.Name);
                 List<Appointment> appointments = AvailableAppointments[doctors];
+                
                 for (int i = 0; i < AvailableAppointments[doctors].Count; i++)
                 {
-                    appointments[i].DisplayAppointmentDetails();
-                    
+                        appointments[i].DisplayAppointmentDetails();
+
                 }
                 Console.WriteLine("**************************************************");
             }
