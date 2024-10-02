ed99f48d67303a9d8f5c3c98982cb8f62951ba9f
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-02 15:41:42 +0400

    Appointment class bug fixing .

--- Appointment.cs
+++ Appointment.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SimpleHospitalManagementSystem
 {
@@ -12,34 +13,38 @@ namespace SimpleHospitalManagementSystem
         //Attributes
 
         public Patient Patient =null;
-        public DateTime? AppointmentDate = null ;
-        public TimeSpan? AppointmentTime = null;
+        public DateTime AppointmentDate;
+        public TimeSpan AppointmentTime;
         public bool IsBooked = false;
 
         //Methods
         //Schedules an appointment between a patient and a doctor.
-        public void ScheduleAppointment (Patient patient, DateTime date,TimeSpan time)
+       
+        public void ScheduleAppointment (Patient patient, DateTime date,TimeSpan time,bool isbooked)
         {
-            Patient = patient;
-            AppointmentDate = date.Date;
-            AppointmentTime = time;
-            IsBooked = true;
+                Patient = patient;
+                AppointmentDate = date.Date;
+                AppointmentTime = time;
+                IsBooked = isbooked;
         }
 
         // Cancels the scheduled appointment
-        
-        public void CancelAppointment(out Patient patient, out DateTime? dateTime ,out TimeSpan? time)
+
+        public void CancelAppointment( Patient patient,  DateTime dateTime , TimeSpan time)
         {
-            patient =Patient;
-            dateTime = AppointmentDate;
-            time =  AppointmentTime;
+            patient = null;
+            AppointmentDate = dateTime;
+            AppointmentTime = time;
             IsBooked = false;
         }
 
         //Displays appointment details.
         public void DisplayAppointmentDetails()
         {
-            Console.WriteLine($"Appointment scheduled for {Patient.Name}  on {AppointmentDate.ToString()} at {AppointmentTime.ToString()} "); 
+            if(IsBooked) 
+            Console.WriteLine($"Appointment scheduled for {Patient.Name}  on {AppointmentDate.ToString()} at {AppointmentTime.ToString()} IsBooked: {IsBooked}"); 
+            else 
+                Console.WriteLine($"Appointment not scheduled  on {AppointmentDate.ToString()} at {AppointmentTime.ToString()} IsBooked {IsBooked}");
         }
 
     }
