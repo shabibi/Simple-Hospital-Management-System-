be8bc657c3c1ea28282d4742d87f4eee3eb62185
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-07 15:58:01 +0400

    Adding IPatientCare ,IInPatientCare and IOutPatientCare interface.

---
+++ IPatientCare.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public interface IPatientCare
+    {
+        void ProvidingCare();
+    }
+    public interface IInPatientCare : IPatientCare
+    {
+        void AssignRoom(Room room);
+        void Discharge();
+    }
+
+    public interface IOutPatientCare : IPatientCare
+    {
+        void ScheduleAppointment();
+    }
+}
