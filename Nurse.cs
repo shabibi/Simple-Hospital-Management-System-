c5b16fec67337e94394c5ebeff006b61b4edcf9b
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-08 09:46:09 +0400

    Adding Nurse Class with it's Attributes and Constructor.

---
+++ Nurse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHospitalManagementSystem
+{
+    public class Nurse :Person
+    {
+        //Attributes
+        public int NurseID;
+        public Clinic AssignedClinic;
+        public Specializations Specialization;
+        public List<Patient> AssignedPatients = new List<Patient>();
+
+        //Constructor
+        public Nurse (int NurseID,string name, int age, Gender gender, Clinic AssignedClinic, Specializations Specialization) :base(name,age,gender)
+        {
+            this.NurseID = NurseID;
+            this.AssignedClinic = AssignedClinic;
+            this.Specialization = Specialization;
+
+        }
+
+
+    }
+}
